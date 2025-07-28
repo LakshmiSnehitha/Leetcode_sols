0f657e2b1f611bada20ba45cbbe5e39dd7d05e47
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-07-28 15:29:23 +0530

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/28/2025, 3:29:22 PM
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i=1;i<nums.size();i++)
+        {
+            nums[i]=nums[i]+nums[i-1];
+        }
+    return nums;
+    }
+};
