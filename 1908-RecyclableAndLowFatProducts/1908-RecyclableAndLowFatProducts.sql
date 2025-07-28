7f9c07f823a670f36c90c3771c5afbbd78fb5fdf
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-07-28 15:29:15 +0530

    Create 1908-RecyclableAndLowFatProducts

---
+++ 1908-RecyclableAndLowFatProducts/1908-RecyclableAndLowFatProducts.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/28/2025, 3:29:14 PM
+# Write your MySQL query statement below
+select product_id from products where low_fats='Y' AND recyclable='Y';
+
+#select product_id from Products where low_fats='Y' And recyclable='Y'
