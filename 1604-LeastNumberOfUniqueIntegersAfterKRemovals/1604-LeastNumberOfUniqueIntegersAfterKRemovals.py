aa9db7160e114c9931c355427a01d545855617e2
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-07-28 15:29:21 +0530

    Create 1604-LeastNumberOfUniqueIntegersAfterKRemovals

---
+++ 1604-LeastNumberOfUniqueIntegersAfterKRemovals/1604-LeastNumberOfUniqueIntegersAfterKRemovals.py
@@ -0,0 +1,17 @@
+# Last updated: 7/28/2025, 3:29:20 PM
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        d={}
+        for i in arr:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        n = len(d)
+        for j in sorted(d.values()):
+            k-=j
+            if(k<0):
+                return n
+            else:
+                n-=1
+        return n
