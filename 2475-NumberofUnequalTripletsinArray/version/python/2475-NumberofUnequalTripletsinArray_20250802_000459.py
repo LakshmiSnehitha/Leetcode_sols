23e8d7ffffa67b0a2dbd63803d33fe2a5f781c3a
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-08-02 00:04:59 +0530

    Create 2475-NumberofUnequalTripletsinArray

---
+++ 2475-NumberofUnequalTripletsinArray/version/python/2475-NumberofUnequalTripletsinArray_20250802_000459.py
@@ -0,0 +1,13 @@
+# Last updated: 8/2/2025, 12:04:59 AM
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        c=0
+        n=len(nums)
+        for i in range (n):
+            for j in range(i+1,n):
+                for k in range(j+1,n):
+                    if nums[i]!=nums[j] and nums[j]!=nums[k] and nums[i]!=nums[k]:
+                        c=c+1
+        return c
+
+        
