3cd9248e0eacd6b936d2752f241ed9cccf72d9e3
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-08-04 19:27:55 +0530

    Create 1848-SumOfUniqueElements

---
+++ 1848-SumOfUniqueElements/1848-SumOfUniqueElements.py
@@ -0,0 +1,20 @@
+# Last updated: 8/4/2025, 7:27:54 PM
+# class Solution:
+#     def sumOfUnique(self, nums: List[int]) -> int:
+#         d={}
+#         for i in nums:
+#             if i in d:
+#                 d[i]+=1
+#             else:
+#                 d[i]=1
+#          return sum(key for key, val in d.items() if val == 1)    
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        # Now sum only the elements that appeared once
+        return sum(key for key, val in d.items() if val == 1)
