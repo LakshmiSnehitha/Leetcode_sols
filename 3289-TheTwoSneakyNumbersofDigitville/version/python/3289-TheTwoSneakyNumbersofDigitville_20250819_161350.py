cba345a003fb4456e46b3dde7ebae1926605259b
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-08-19 16:13:51 +0530

    Create 3289-TheTwoSneakyNumbersofDigitville

---
+++ 3289-TheTwoSneakyNumbersofDigitville/version/python/3289-TheTwoSneakyNumbersofDigitville_20250819_161350.py
@@ -0,0 +1,15 @@
+# Last updated: 8/19/2025, 4:13:50 PM
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        d={}
+        s=[]
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for key,value in d.items():
+            if value==2:
+                s.append(key)
+        return s
+      
