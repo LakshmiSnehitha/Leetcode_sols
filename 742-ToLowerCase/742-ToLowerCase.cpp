6031e8145b6319936443a9c6aecdea0f1f86279d
Lakshmi Snehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2025-09-03 11:24:50 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.cpp
@@ -0,0 +1,12 @@
+// Last updated: 9/3/2025, 11:24:49 AM
+class Solution {
+public:
+    string toLowerCase(string s) {
+       for(int i=0;i<size(s);i++){
+        s[i]=tolower(s[i]);
+       }
+       return s;
+    }
+   
+};
+
